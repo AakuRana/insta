ac5e55f8304fe80b00382d7ad27f013b7c93c1a6
AakuRana <ralok7323@gmail.com>
2026-03-06 00:00:10 +0530

    Fix path variable usage for static files and views

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files (CSS, images, etc.) from "public" folder
-app.use(express.static(path.join(dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // ---------------------
 // View Engine Setup
 // ---------------------
 app.set("view engine", "ejs");
-app.set("views", path.join(dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 
 // ---------------------
 // Routes
